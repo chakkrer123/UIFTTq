1d7f7e597c2ca7f0cbe1ddf4e64e1833249c3b0d
Kee123 <docghwe31@gmail.com>
2016-09-02 02:18:46 +0700

    Start UI  2Day

--- app/src/main/java/com/example/kee/uiftft/MainActivity.java
+++ app/src/main/java/com/example/kee/uiftft/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.kee.uiftft;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button t1, t2;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t1 = (Button) findViewById(R.id.button);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Timer.class);
+                startActivity(intent);
+
+            }
+        });
+        t2 = (Button) findViewById(R.id.button2);
+        t2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, Timer2.class);
+                startActivity(intent1);
+            }
+        });
+
+
+
     }
 }
